8c99c792f071c5a9e34603de01596c943beb42ea
pavel kuznetsov <pavel.kuznetsov@lamoda.ru>
2015-06-18 13:45:53 +0300

    fixes

--- iphotoweb-app/src/main/java/ru/stierus/iphoto/Main.java
+++ iphotoweb-app/src/main/java/ru/stierus/iphoto/Main.java
@@ -1,33 +1,37 @@
 package ru.stierus.iphoto;
 
 import ru.stierus.iphoto.web.controller.TestListener;
+import ru.stierus.iphotoweb.service.model.PhotoLibrary;
 import ru.stierus.iphotoweb.service.parser.IPhotoLibraryParser;
 import ru.stierus.iphotoweb.service.service.PhotoLibraryMonitorChangeDataService;
 import ru.stierus.iphotoweb.service.service.PhotoLibraryParserService;
 import ru.stierus.iphotoweb.service.service.PhotoLibraryService;
 
+import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         String path = "/Users/pavel/coding/iphoto/data/test.photolibrary/AlbumData.xml";
-        PhotoLibraryService photoLibraryService = new PhotoLibraryService();
-        photoLibraryService.addListener(new TestListener());
+//        PhotoLibraryService photoLibraryService = new PhotoLibraryService();
+//        photoLibraryService.addListener(new TestListener());
+//
+//        PhotoLibraryMonitorChangeDataService monitorService = new PhotoLibraryMonitorChangeDataService();
+//        monitorService.addLibrary(Paths.get(path));
+//
+//        PhotoLibraryParserService parseService = new PhotoLibraryParserService(
+//                photoLibraryService,
+//                new IPhotoLibraryParser(),
+//                monitorService
+//        );
+//
+//        parseService.start();
 
-        PhotoLibraryMonitorChangeDataService monitorService = new PhotoLibraryMonitorChangeDataService();
-        monitorService.addLibrary(Paths.get(path));
+        IPhotoLibraryParser parser = new IPhotoLibraryParser();
 
-        PhotoLibraryParserService parseService = new PhotoLibraryParserService(
-                photoLibraryService,
-                new IPhotoLibraryParser(),
-                monitorService
-        );
-
-        parseService.start();
-
-
+        PhotoLibrary library = parser.parseAlbumData(Paths.get(path).toFile());
 
         System.out.println("result: ");
-//        System.out.println(library.toString());
+        System.out.println(library.toString());
     }
 }
